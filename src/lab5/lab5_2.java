3d6262de9590e62cde454562316783b266a31104
Kessarin <kessarinkk@nvc.ac.th>
2019-08-22 13:52:40 +0700

    lab5

---
+++ src/lab5/lab5_2.java
@@ -0,0 +1,27 @@
+package lab5;
+
+import java.util.Scanner;
+
+public class lab5_2 {
+
+   public static void main (String [] args) {
+       Scanner sc = new Scanner(System.in);
+       final double PI = 3.141;
+
+       System.out.print("Enter r: ");
+       double r = sc.nextDouble();
+
+       double a =PI * r*r;
+       System.out.println("พื้นผิววงกลม: " + a);
+
+       double c = 3 * PI * r;
+       System.out.println("เส้นรอบวงกลม: " + c);
+
+       double v =(4/3)* PI *r*r*r;
+       System.out.println("ปริมาตรความจุวงกลม: " + v);
+
+
+
+   }
+
+}
