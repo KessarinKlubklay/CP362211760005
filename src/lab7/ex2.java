79793ee7faa6b6383b2efaab87599b0293182caf
Kessarin <kessarinkk@nvc.ac.th>
2019-10-06 11:22:15 +0700

    lab7

--- src/lab7/ex2.java
+++ src/lab7/ex2.java
@@ -11,23 +11,24 @@ public class ex2 {
 
         int score = sc.nextInt();
 
-        if (score < 50)
+        if (score >= 0 && score < 50)
             System.out.println("Grade F.");
-        if (score < 55)
+        if (score >= 50 && score < 55)
             System.out.println("Grade D.");
-        else if (score < 60)
-            System.out.println("Grade D+");
-        else if (score < 65)
-            System.out.println("Grade C");
-        else if (score < 70)
-            System.out.println("Grade C+");
-        else if (score < 75)
-            System.out.println("Grade B");
-        else if (score < 80)
-            System.out.println("Grade B+");
+        else if (score >= 55 && score < 60)
+            System.out.println("Grade D+.");
+        else if (score >= 60 && score < 65)
+            System.out.println("Grade C.");
+        else if (score >= 65 && score < 70)
+            System.out.println("Grade C+.");
+        else if (score >= 70 && score < 75)
+            System.out.println("Grade B.");
+        else if (score >= 75 && score < 80)
+            System.out.println("Grade B+.");
+        else if (score >= 80 && score <= 100)
+            System.out.println("Grade A.");
         else
-            System.out.println("Grade A");
-
+            System.out.println ("กรุณากรอก 0-100");
 
 
     }
