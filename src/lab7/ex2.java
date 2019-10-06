f3adadf6523b98e969eda436f276e1266519982b
Kessarin <kessarinkk@nvc.ac.th>
2019-10-06 15:10:45 +0700

    lab7

--- src/lab7/ex2.java
+++ src/lab7/ex2.java
@@ -4,32 +4,38 @@ import java.util.Scanner;
 
 public class ex2 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        int score;
 
-        System.out.print("Enter your score,"+
+        do {
+            System.out.print("Enter your score," +
                 "and I will tell you grade :");
+            score = sc.nextInt();
 
-        int score = sc.nextInt();
+                if (score >= 0 && score < 50)
+                    System.out.println("Grade F.");
+                else if (score >= 50 && score < 55)
+                    System.out.println("Grade D.");
+                else if (score >= 55 && score < 60)
+                    System.out.println("Grade D+.");
+                else if (score >= 60 && score < 65)
+                    System.out.println("Grade C.");
+                else if (score >= 65 && score < 70)
+                    System.out.println("Grade C+.");
+                else if (score >= 70 && score < 75)
+                    System.out.println("Grade B.");
+                else if (score >= 75 && score < 80)
+                    System.out.println("Grade B+.");
+                else if (score >= 80 && score <= 100)
+                    System.out.println("Grade A.");
+                else
+                    System.out.println("กรุณากรอก 0-100");
 
-        if (score >= 0 && score < 50)
-            System.out.println("Grade F.");
-        if (score >= 50 && score < 55)
-            System.out.println("Grade D.");
-        else if (score >= 55 && score < 60)
-            System.out.println("Grade D+.");
-        else if (score >= 60 && score < 65)
-            System.out.println("Grade C.");
-        else if (score >= 65 && score < 70)
-            System.out.println("Grade C+.");
-        else if (score >= 70 && score < 75)
-            System.out.println("Grade B.");
-        else if (score >= 75 && score < 80)
-            System.out.println("Grade B+.");
-        else if (score >= 80 && score <= 100)
-            System.out.println("Grade A.");
-        else
-            System.out.println ("กรุณากรอก 0-100");
+            }while (!(score >=0&& score <=100));
 
 
+
+
+        }
     }
-}
